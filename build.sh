60901f45c870668169f6c5c25b215357897fab07
kaneki <kaneki@catchme.com.cn>
2018-01-03 12:51:39 +0800

    添加新接口

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-protoc --proto_path=./protos --php_out=./src  --grpc_out=./src --plugin=protoc-gen-grpc=/usr/local/bin/grpc_php_plugin ./protos/$1.proto
+protoc --proto_path=./protos --php_out=./src/php  --grpc_out=./src/php --plugin=protoc-gen-grpc=/usr/local/bin/grpc_php_plugin ./protos/$1.proto
